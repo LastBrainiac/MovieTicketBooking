22dc673cdaf7fb094e2f14f2ddfc72e1af010b0f
LastBrainiac <attila.ludanszki@gmail.com>
2023-03-30 21:16:39 +0200

    Default controllers deleted from APIs

---
+++ MTBS.MovieCatalogAPI/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MTBS.MovieCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("GetAllMovies");
+        }
+    }
+}
